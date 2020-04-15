a2819b47597dbde52c96249931d4c362cedf0268
rmdhfz <hfzrmd@gmail.com>
2020-04-15 16:39:39 +0700

    initial commit

---
+++ fib.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// fibonacci adalah sebuah fungsi yang mengembalikan sebuah fungsi yang
+// mengembalikan sebuah integer.
+func fibonacci() func() int {
+	f2, f1 := 0, 1
+	return func() int {
+		f := f2
+		f2, f1 = f1, f+f1
+		return f
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
